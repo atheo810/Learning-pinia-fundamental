bbf5a9166e8fcd01032a90e3e2c44f1423ef3b5b
atheo810 <atheos810@gmail.com>
2024-02-05 13:26:14 +0800

    feat: TodoForm

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { UseTodoListStore } from "@/stores/todoList";
+
+// reactive state
+const todo = ref("");
+
+// use store in UseTodoListStore
+const store = UseTodoListStore();
+
+function addItemAndClear(item) {
+  if (item.length === 0) {
+    return;
+  }
+  store.addTodo(item);
+  todo.value = "";
+}
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input type="text" v-model="todo" />
+      <button>Add</button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
