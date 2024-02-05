77aecb1c9339b5a076ca3f1b5c96522384a92d97
atheo810 <atheos810@gmail.com>
2024-02-05 09:47:41 +0800

    feat: TodoApp

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TodoForm from "./TodoForm.vue";
+import TodoList from "./TodoList.vue";
+</script>
+
+<template>
+  <div class="todo-app">
+    <TodoForm />
+    <TodoList />
+  </div>
+</template>
+
+<style scoped></style>
