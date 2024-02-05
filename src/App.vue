abfc5a070943e0c1f649d8c818daf748b615597a
atheo810 <atheos810@gmail.com>
2024-02-05 13:44:06 +0800

    feat: added style

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,18 @@ import TodoApp from "@/components/TodoApp.vue";
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Helvetica, sans-serif;
+  background-color: #597d9a;
+  display: flex;
+  justify-content: center;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
